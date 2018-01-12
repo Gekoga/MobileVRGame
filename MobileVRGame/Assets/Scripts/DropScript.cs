cda91c855420c36371c9f7b937f2888d8e27bd94
Berber Bouma <206841@edu.rocfriesepoort.nl>
2018-01-12 16:22:03 +0100

    verder gegaan met inventory, verder gegaan met button

---
+++ MobileVRGame/Assets/Scripts/DropScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropScript : MonoBehaviour {
+
+    public Inventory inventory;
+
+    public GameObject keySpace;
+    public bool hasKey;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.O))
+        {
+            CollectKey();
+        }
+	}
+
+    public void CollectKey()
+    {
+        if (inventory.keys.Count >= 1)
+        {
+            keySpace = inventory.gameObject;
+        }
+
+        else if (inventory.keys.Count <= 0)
+        {
+            return;
+        }
+    }
+
+}
